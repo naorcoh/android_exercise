c878a67e4f25f7a70815e398dfca2467665adc4d
Naorcoh <naorcohen92@gmail.com>
2022-08-23 00:06:51 +0300

    init new Android project

---
+++ app/src/main/java/com/example/andoridexercise_1/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.andoridexercise_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.TextureView
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun getName(name: String): String {
+        var name = name
+        return name
+    }
+
+    fun onButtonClick(view: View) {
+        val textView: TextView = findViewById(R.id.hello_text)
+        val editTextCon = getEditText()
+
+    }
+
+
+    fun getEditText(): String {
+        val editText: EditText = findViewById(R.id.edit_text_1)
+        return editText.text.toString()
+    }
+
+
+}
